5821c39b59d045efa390e963380628a56dc90db8
Ben <bengetch@gmail.com>
2019-02-25 16:46:46 -0500

    setup repo

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name             = 'policy-engine',
+    version          = '0.0.0.1',
+    packages         = find_packages(),
+    license          = 'MIT',
+    url              = 'https://github.com/cici-conclave/policy-engine',
+    description      = 'Infrastructure for defining and running large data workflows against multiple backends.',
+    long_description = open('README.md').read(),
+)
