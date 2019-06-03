e9655f4d6653f6c5d933fcca241cb854876139be
Ben <bengetch@gmail.com>
2019-06-03 10:44:32 -0400

    fix typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version          = '0.0.0.1',
     packages         = find_packages(),
     license          = 'MIT',
-    url              = 'https://github.com/cici-conclave/policy-engine',
-    description      = 'Infrastructure for defining and running large data workflows against multiple backends.',
+    url              = 'https://github.com/cici-conclave/policy_engine',
+    description      = 'Policy engine for verifying workflows',
     long_description = open('README.md').read(),
 )
