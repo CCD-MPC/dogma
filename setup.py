8ba57ea4b1fb572d015e1178fbb03d8b4e511936
bengetch <bengetch@gmail.com>
2019-09-11 23:00:07 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name             = 'policy_engine',
+    name             = 'dogma',
     version          = '0.0.0.1',
     packages         = find_packages(),
     license          = 'MIT',
-    url              = 'https://github.com/cici-conclave/catechism',
+    url              = 'https://github.com/cici-conclave/dogma',
     description      = 'Policy engine for verifying workflows',
     long_description = open('README.md').read(),
 )
