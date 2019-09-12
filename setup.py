805f117ec2940775b2305e9e15e83786f9ebcc49
bengetch <bengetch@gmail.com>
2019-09-11 22:44:24 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version          = '0.0.0.1',
     packages         = find_packages(),
     license          = 'MIT',
-    url              = 'https://github.com/cici-conclave/policy_engine',
+    url              = 'https://github.com/cici-conclave/catechism',
     description      = 'Policy engine for verifying workflows',
     long_description = open('README.md').read(),
 )
